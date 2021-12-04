165d833e384463ce5dacc931bd75b1b07ee63844
Jayfromthe13th <83922342+Jayfromthe13th@users.noreply.github.com>
2021-12-04 15:30:00 -0700

    Added fuzz testing

---
+++ SimpleEchidnaTest.sol
@@ -0,0 +1,10 @@
+import"./Controller.sol";
+
+contract TestController is Controller {
+    function echindna_testpass() public view returns (bool) {
+        return true;
+    }
+       function echindna_testfails() public view returns (bool) {
+        return false;
+}
+   }
