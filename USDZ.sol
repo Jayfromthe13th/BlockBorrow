5092929a0301156e3f82d0d90307f1a5142160a8
Jayfromthe13th <83922342+Jayfromthe13th@users.noreply.github.com>
2022-07-14 14:35:22 -0600

    Add files via upload

---
+++ USDZ.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import './interfaces/IUSDZ.sol';
+
+contract USDZ is ERC20, IUSDZ {
+    // contract with permission to mint/burn tokens
+    address public controller;
+
+    constructor(
+        address _controller,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        controller = _controller;
+    }
+
+    function mint(address _account, uint256 _amount)
+        external
+        override
+        onlyController()
+    {
+        _mint(_account, _amount);
+
+        emit Mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount)
+        external
+        override
+        onlyController()
+    {
+        _burn(_account, _amount);
+
+        emit Burn(_account, _amount);
+    }
+
+    // Matching USDC's 6 decimals as USDZ will be pegged to USDC
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    modifier onlyController() {
+        require(msg.sender == controller, "only controller has permission");
+        _;
+    }
+}
+contract TestFuzz is USDZ {
+    function echindna_testpass() public view returns (bool) {
+        return true;
+    } }
